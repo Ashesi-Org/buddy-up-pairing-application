b997d992380d3cdd5ad36b95c15978d900afac19
ngohamah <ngoh.amah@ashesi.edu.gh>
2023-03-28 21:23:20 +0000

    completed UI

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ app = Flask(__name__, template_folder='templates')
 @app.route("/")
 def home():
     return render_template("home.html")
-    
-@app.route("/about")
-def about():
-    return render_template("about.html")
+
+@app.route("/progress")
+def progress():
+    return render_template("progress.html")
+
+@app.route("download")
+def download():
+    return render_template("download.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
