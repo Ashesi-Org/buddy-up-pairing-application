342f210d82cf83fff6a8401e3f57ed3275fbdc4e
ngohamah <ngoh.amah@ashesi.edu.gh>
2023-04-20 10:33:57 +0000

    error with downloading the zip file

--- model/readfromfile.py
+++ model/readfromfile.py
@@ -1,14 +1,10 @@
-#Required packages to install
-
-#!pip install openpyxl
-#!pip install odfpy
-#!pip install xlrd
-
 import pandas as pd 
 
 #reading student profiles from excel documents
-continuingstudents = pd.read_excel("../controller/uploads/continuing.xlsx", index_col=0, sheet_name="continuing students")
-freshmen = pd.read_excel("../controller/uploads/freshers.xlsx", index_col=0, sheet_name="freshers")
+continuingstudents = pd.read_excel("../controller/uploads/continuing.xlsx",\
+                                index_col=0, sheet_name="continuing students")
+freshmen = pd.read_excel("../controller/uploads/freshers.xlsx", \
+                                index_col=0, sheet_name="freshers")
 
 
 '''sorting dataframe such that continuing students who specified \\Any\\
